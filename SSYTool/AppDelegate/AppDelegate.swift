267d5450a1b5249c726be39b68754b5c34aae83f
Yunis <itsongshangyong@gmail.com>
2021-02-25 16:24:05 +0800

    第一次上传

--- SSYTool/AppDelegate/AppDelegate.swift
+++ SSYTool/AppDelegate/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
-		print("delete scencs")
+		print("delete scencs ")
 
 		return true
 	}
